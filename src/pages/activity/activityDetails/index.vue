7998e82817bdeac21628438aff48b452cdfab568
xbbSu1y <876133937@qq.com>
2018-06-06 14:19:21 +0800

    201806061420

--- src/pages/activity/activityDetails/index.vue
+++ src/pages/activity/activityDetails/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="activity-details">
     <v-head></v-head>
-    <div class="wrapper" ref="wrapper">
+    <div class="wrapper" ref="wrapper" :style="{height: winHeight}">
       <section class="content" ref="content">
         <v-banner :style="{ height: bannerHeight }" :listImg="listImg" @chooseItem="chooseItem"></v-banner>
         <v-title></v-title>
@@ -11,9 +11,10 @@
         <v-introduction></v-introduction>
         <div class="interval"></div>
         <v-comment></v-comment>
-        <v-footer></v-footer>
       </section>
     </div>
+    <v-footer></v-footer>
+    <img v-show="showToTopBtn" @click="scrollToTop()" class="to-top-icon" src="../to_top_icon.png">
   </div>
 </template>
 
@@ -25,8 +26,9 @@ import message from './message/message.vue'
 import introduction from './introduction/introduction.vue'
 import comment from './comment/comment.vue'
 import footer from './footer/footer.vue'
+import BScroll from 'better-scroll'
 export default {
-  name: '',
+  name: 'activity-details',
   components: {
     'v-head': head,
     'v-banner': banner,
@@ -38,15 +40,32 @@ export default {
   },
   data () {
     return {
+      // 屏幕高度
+      winHeight: window.innerHeight - 45 + 'px',
+      // 初始化Bscroll
+      scroller: '',
+      // BScroll滚动高度
+      scrollHeight: 0,
       // banner高度
       bannerHeight: window.innerWidth * 0.6 + 'px',
       // banner图片数组
-      bannerImages: []
+      bannerImages: [],
+      // 是否可以刷新
+      reload: true,
+      // 是否可以加载更多
+      loadMore: true,
+      // 页码
+      pageNum: 0
     }
   },
   created () {},
-  mounted () {},
+  mounted () {
+    this.loadData()
+  },
   computed: {
+    showToTopBtn () {
+      return this.scrollHeight > window.innerHeight
+    },
     listImg () {
       let list = []
       if (this.bannerImages.length) {
@@ -66,11 +85,51 @@ export default {
     }
   },
   methods: {
+    loadData () {
+      this.$nextTick(() => {
+        this.initBetterScroll()
+      })
+    },
+    // 初始化 scroller
+    initBetterScroll () {
+      if (!this.scroller) {
+        this.scroller = new BScroll(this.$refs.wrapper, {
+          probeType: 3,
+          click: true
+        })
+        this.listenScroll()
+      } else {
+        this.scroller.refresh()
+      }
+    },
+    // 监听BScroll滑动
+    listenScroll () {
+      this.scroller.on('scroll', pos => {
+        if (pos.y >= 100 && this.reload) {
+          console.log('上拉到顶部刷新数据')
+        }
+        this.scrollHeight = -pos.y
+      })
+    },
+    // 点击回到顶部
+    scrollToTop () {
+      this.scroller.scrollTo(0, 0, 500)
+    },
+    // 点击banner
     chooseItem (param) {
       console.log(param)
-    }
+    },
   },
-  watch: {}
+  watch: {
+    scrollHeight (val) {
+      // 下拉到最下面加载更多
+      if (val > this.$refs.content.offsetHeight - window.innerHeight && this.loadMore) {
+        this.loadMore = false
+        this.pageNum += 1
+        console.log('下拉到底部加载更多')
+      }
+    }
+  }
 }
 </script>
 
