5ff0976e0926ea51009d2b8ef370613a768bb715
xbbSu1y <876133937@qq.com>
2018-12-05 09:34:56 +0800

    20181205

---
+++ src/components/spyp/spypPlayer/spypPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+    export default {
+        name: '',
+        components: {},
+        data () {
+            return {}
+        },
+        created () {
+        },
+        mounted () {
+        },
+        computed: {},
+        methods: {},
+        watch: {}
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import ''
+</style>
